d7a45218c5290b95409f4401fbac10a46e53db01
falldeaf <falldeaf@gmail.com>
2013-06-21 12:31:30 -0400

    Added hard-coded path to daily_lamp_update.py script

--- daily_lamp_update.py
+++ daily_lamp_update.py
@@ -4,7 +4,7 @@ import cPickle as pickle
 
 
 weather = reports.weather_report()
-pickle.dump( weather, open( "weather.p", "wb") )
+pickle.dump( weather, open( "/home/pi/scripts/weather.p", "wb") )
 
 surf = reports.surf_report()
-pickle.dump( surf, open( "surf.p", "wb") )
+pickle.dump( surf, open( "/home/pi/scripts/surf.p", "wb") )
